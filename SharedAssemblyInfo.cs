95eca9f9ca838061a7115cc509bf17584ef9e71c
Robert Snyder <immeraufdemhund@hotmail.com>
2017-07-17 13:45:35 -0400

    Add project files.

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Goldbox.Engine")]
+[assembly: AssemblyCompany("Robert Snyder")]
+[assembly: AssemblyProduct("Goldbox.Engine")]
+[assembly: AssemblyCopyright("Copyright © Robert Snyder 2017")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
